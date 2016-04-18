8ddffb94088bac43422a4ef387c5914a8066517f
Jan Walzer <jw@1und1.de>
2016-04-18 12:24:57 +0200

    use the bashlib from /opt/bashlib

--- bin/dnssec-check.bash
+++ bin/dnssec-check.bash
@@ -1,16 +1,12 @@
 #!/bin/bash
 #
 
-#BASHLIB_DEBUG="TRUE"
-
-[ -r "/usr/local/lib/bashlib/init.bash" ] && . "/usr/local/lib/bashlib/init.bash"
+[ -r "/opt/bashlib/lib/init.bash" ] && . "/opt/bashlib/lib/init.bash"
 if [ "$INCLUDED_INIT" != "$RUNTOKEN" ]
     then
         echo "Failed to load libraries"
         exit 1
     fi
-
-[[ "$1" = "-d" ]] && BASHLIB_DEBUG="TRUE"
 
 INCLUDE "prereqs"
 INCLUDE "config"
@@ -21,7 +17,7 @@ MYPATH="$(dirname $0)"
 ITODNS_LIBS="$MYPATH/../lib"
 MODULES_DIR="$ITODNS_LIBS/modules"
 
-CONFIG_TRY_LOAD /etc/itodns.conf ~/.itodns.conf
+CONFIG_TRY_LOAD "$MYPATH/itodns.conf"  /etc/itodns.conf ~/.itodns.conf
 
 LIBDIR_ADD "$ITODNS_LIBS"
 
@@ -39,12 +35,10 @@ CONFIG_TRY_LOAD /etc/itodns.conf ~/.itodns.conf
 
 CHECK_PREREQS
 
-#BASHLIB_DEBUG="TRUE"
 
 CHECKDOMAINS "mail.com" "ui-r.com" "caramail.com" "oneandone.net"
 
 VERIFY_SEC NS ui-r.com || ERR "could not validate"
 VERIFY_SEC TXT ui-r.com || ERR "could not validate"
-#VERIFY_SEC NSEC3 h69d0ht05fconsv11efmr0b3qrv31903.ui-r.com.
 
 LOG "Done"
